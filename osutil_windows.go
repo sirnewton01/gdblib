772aff99a91161cae47caa031c7c6dedafce9a83
Chris McGee <sirnewton_01@yahoo.ca>
2013-09-02 20:39:37 -0400

    Convert ListSeparator into a string to use in Split.

--- osutil_windows.go
+++ osutil_windows.go
@@ -20,7 +20,7 @@ var (
 
 func init() {
 	gopath := build.Default.GOPATH
-	gopaths := strings.Split(gopath, filepath.ListSeparator)
+	gopaths := strings.Split(gopath, string(filepath.ListSeparator))
 	for _,path := range(gopaths) {
 		p := path + "\\src\\github.com\\sirnewton01\\gdblib\\SendSignal.exe"
 		_,err := os.Stat(p)
